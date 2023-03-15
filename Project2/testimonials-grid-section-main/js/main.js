b5d3f34e6166c6036d1d853e2ef6aca28699184b
JRW <jakub.robert.w@gmail.com>
2023-03-15 22:25:48 +0100

    Meh v.1

---
+++ Project2/testimonials-grid-section-main/js/main.js
@@ -0,0 +1,9 @@
+// there is nothing special
+
+// var cards = document.querySelectorAll('.clickcard')
+
+// ;[...cards].forEach(card => {
+// 	card.addEventListener('click', function () {
+// 		card.classList.toggle('tile-back')
+// 	})
+// })
